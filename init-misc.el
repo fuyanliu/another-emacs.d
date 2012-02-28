1144df942e597757cf34911cf399ee6799d1e348
sylvester <cedsyd@gmail.com>
2012-02-28 20:06:28 +0800

    fix name of display time face.

--- init-misc.el
+++ init-misc.el
@@ -31,21 +31,21 @@
 ; from RobinH
 ;Time management
 (display-time)
-(defface display-time-face
+(defface sydi-display-time-face
   '((((type x w32 mac))
      ;; #060525 is the background colour of my default face.
      (:foreground "#7F00FF" :inherit bold))
     (((type tty))
      (:foreground "blue")))
   "Face used to display the time in the mode line."
-  :group 'display-time)
+  :group 'sydi-display-time)
 
 ;; This causes the current time in the mode line to be displayed in
-;; `egoge-display-time-face' to make it stand out visually.
+;; `display-time-face' to make it stand out visually.
 (setq display-time-string-forms
       '("["
         (propertize (concat 24-hours ":" minutes)
- 		    'face 'display-time-face)
+ 		    'face 'sydi-display-time-face)
         "]"))
 
 (global-set-key [f8] 'calendar)
